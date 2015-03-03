7788f69ce2b23c736e15808bf3714ada80b8adaf
Rachel Brindle <you@subluminal.net>
2015-03-02 22:22:48 -0800

    Add cliffs to the landscape

--- Swift-Crossing/LandScape.swift
+++ Swift-Crossing/LandScape.swift
@@ -25,6 +25,29 @@ class LandScape {
         floor.firstMaterial = mat
         let node = SCNNode(geometry: floor)
         node.physicsBody = SCNPhysicsBody.staticBody()
+
+        for i in 0..<3 {
+            let wall = SCNPlane(width: 15, height: 100)
+            let wallMaterial = SCNMaterial()
+            wallMaterial.diffuse.contents = NSColor.brownColor()
+            wall.firstMaterial = wallMaterial
+            let n = SCNNode(geometry: wall)
+
+            let angle = CGFloat(M_PI_2)
+
+            if i == 0 { // left
+                n.position = SCNVector3Make(-50, 7.5, 0)
+                n.eulerAngles = SCNVector3Make(-angle, 0, -angle)
+            } else if i == 1 { // back
+                n.position = SCNVector3Make(0, 7.5, -50)
+                n.eulerAngles = SCNVector3Make(0, 0, angle)
+            } else if i == 2 { // right
+                n.position = SCNVector3Make(50, 7.5, 0)
+                n.eulerAngles = SCNVector3Make(angle, 0, -angle)
+            }
+            node.addChildNode(n)
+        }
+
         return node
     }
 }
--- Swift-CrossingTests/LandScapeSpec.swift
+++ Swift-CrossingTests/LandScapeSpec.swift
@@ -23,6 +23,33 @@ class LandScapeSpec: QuickSpec {
                 }
             }
 
+            it("have 3 brown walls (cliffs) on the back and sides that each extend up 10 units") {
+                expect(subject.contents.childNodes.count).to(equal(3))
+                let childNodes = subject.contents.childNodes
+                for (i,x) in enumerate(childNodes) {
+                    expect(x.geometry).to(beAnInstanceOf(SCNPlane.self))
+                    if let g = x.geometry as? SCNPlane {
+                        expect(g.firstMaterial).toNot(beNil())
+                        if let color = g.firstMaterial?.diffuse.contents as? NSColor {
+                            expect(color).to(equal(NSColor.brownColor()))
+                        }
+                        expect(g.width).to(equal(15))
+                        expect(g.height).to(equal(100))
+                    }
+                    let angle = CGFloat(M_PI_2)
+                    if i == 0 {
+                        expect(SCNVector3EqualToVector3(x.position, SCNVector3Make(-50, 7.5, 0))).to(beTruthy())
+                        expect(SCNVector3EqualToVector3(x.eulerAngles, SCNVector3Make(-angle, 0, -angle))).to(beTruthy())
+                    } else if i == 1 {
+                        expect(SCNVector3EqualToVector3(x.position, SCNVector3Make(0, 7.5, -50))).to(beTruthy())
+                        expect(SCNVector3EqualToVector3(x.eulerAngles, SCNVector3Make(0, 0, angle))).to(beTruthy())
+                    } else if i == 2 {
+                        expect(SCNVector3EqualToVector3(x.position, SCNVector3Make(50, 7.5, 0))).to(beTruthy())
+                        expect(SCNVector3EqualToVector3(x.eulerAngles, SCNVector3Make(angle, 0, -angle))).to(beTruthy())
+                    }
+                }
+            }
+
             it("should attach a static physics body") {
                 expect(subject.contents.physicsBody).toNot(beNil())
                 if let body = subject.contents.physicsBody {
