221492377d7a575ac2ffb4333b16eca43eee232a
Rachel Brindle <you@subluminal.net>
2015-03-08 11:18:22 -0700

    ocean now has a wall preventing people from walking on it

--- Swift-Crossing/Ocean.swift
+++ Swift-Crossing/Ocean.swift
@@ -17,7 +17,15 @@ class Ocean: SCNNode {
         let oceanNode = SCNNode(geometry: ocean)
         oceanNode.name = "Ocean"
         oceanNode.eulerAngles = SCNVector3Make(CGFloat(-M_PI_2), 0, 0)
+        oceanNode.physicsBody = SCNPhysicsBody.staticBody()
         addChildNode(oceanNode)
+
+        let wallNode = SCNNode()
+        wallNode.name = "Wall"
+        let wall = SCNPlane(width: 100, height: 10)
+        wallNode.physicsBody = SCNPhysicsBody(type: .Static, shape: SCNPhysicsShape(geometry: wall, options: nil))
+        addChildNode(wallNode)
+
     }
 
     required init(coder: NSCoder) {
--- Swift-CrossingTests/OceanSpec.swift
+++ Swift-CrossingTests/OceanSpec.swift
@@ -14,9 +14,14 @@ class OceanSpec: QuickSpec {
             let n = subject.childNodeWithName("Ocean", recursively: false)
             expect(n).toNot(beNil())
             if let node = n {
+                expect(node.physicsBody).toNot(beNil())
+                if let pb = node.physicsBody {
+                    expect(pb.type).to(equal(SCNPhysicsBodyType.Static))
+                }
                 expect(node.eulerAngles).to(equal(SCNVector3Make(CGFloat(-M_PI_2), 0, 0)))
                 expect(node.geometry).to(beAnInstanceOf(SCNPlane.self))
                 if let geom = node.geometry as? SCNPlane {
+                    expect(geom.materials?.count).to(equal(1))
                     expect(geom.firstMaterial).toNot(beNil())
                     if let mat = geom.firstMaterial {
                         let color = mat.diffuse.contents as! NSColor
@@ -29,7 +34,16 @@ class OceanSpec: QuickSpec {
         }
 
         it("should have an invisble wall on the north side preventing anything from going through it.") {
-            // Yeah, I have no idea how to test this :/
+            let n = subject.childNodeWithName("Wall", recursively: false)
+            expect(n).toNot(beNil())
+            if let node = n {
+                expect(node.physicsBody).toNot(beNil())
+                if let pb = node.physicsBody {
+                    expect(pb.type).to(equal(SCNPhysicsBodyType.Static))
+                    expect(pb.physicsShape).toNot(beNil())
+                }
+                expect(node.geometry).to(beNil())
+            }
         }
     }
 }
